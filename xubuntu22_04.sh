4776d3c23e2489149b15b80fecbfc3cce7208217
Красимир Беров <berov@cpan.org>
2022-07-10 09:51:23 +0300

    Initial commit

---
+++ xubuntu22_04.sh
@@ -0,0 +1,14 @@
+# This is a log of executed commands, which can be sourced on your machine.
+# The commands were collected over the internet.
+# I log them for my self to execute ithem later on other newly installed machines.
+# 2022-07-10 00:03 EEST
+# Install the gnome browser to get to this page or use links
+# sudo apt install epiphany-browser
+# List all the snaps installed on your system
+snap list
+# Remove all installed snaps
+sudo snap remove firefox gnome-3-38-2004 gtk-common-themes
+sudo snap remove  bare
+sudo snap remove  core20
+snap list
+# Remove anything left
